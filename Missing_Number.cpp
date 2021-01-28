42a1acefdca91a5c71ff7ae778f23c87e6efc958
rajanshoo25 <rajanshoo25@gmail.com>
2021-01-29 02:55:10 +0530

    Add 1.2 and 1.3

---
+++ Missing_Number.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fo(i,n) for(ll i=0;i<n;i++)
+ 
+void solve()
+{
+    ll n;
+    cin >> n;
+    int arr[n];
+    fo(i,n-1)
+        cin >> arr[i];
+        
+    sort(arr, arr + n-1);
+    bool f = true;
+    fo(i,n-1)
+    {
+        if(arr[i]!=i+1)
+        {
+            cout << i+1;
+            f = false;
+            break;
+        }
+    }
+    if(f)
+    cout << n;
+}
+ 
+int main()
+{
+    solve();
+}
